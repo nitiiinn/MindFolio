2d551616901b4e0468edc1f0de5de5cc2ad24c9b
nitiiinn <nitin21kothiyal@gmail.com>
2026-06-09 13:36:39 +0530

    fix: revert invalid global replace of use_container_width to width that caused complete app crash

--- ingestion/splitter.py
+++ ingestion/splitter.py
@@ -3,8 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(docs):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=750,
-        chunk_overlap=100,
+        chunk_size=1000,
+        chunk_overlap=200,
     )
 
     chunks = splitter.split_documents(docs)
