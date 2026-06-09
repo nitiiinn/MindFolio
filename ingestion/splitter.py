279f82767d5881e5ba95c704a0e2ae99cbafde47
nitiiinn <nitin21kothiyal@gmail.com>
2026-06-10 00:06:12 +0530

    decreased chunk size to 1250

--- ingestion/splitter.py
+++ ingestion/splitter.py
@@ -3,8 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(docs):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2500,
-        chunk_overlap=400,
+        chunk_size=1500,
+        chunk_overlap=300,
     )
 
     chunks = splitter.split_documents(docs)
